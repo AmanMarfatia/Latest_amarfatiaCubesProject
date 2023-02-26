600bdaaaa6d46e7780ec3725fe277625cbe06535
amanmarfatia <amarfatia@student.bridgew.edu>
2023-02-25 21:41:34 -0500

    new

--- getData.py
+++ getData.py
@@ -1,13 +1,12 @@
 import requests
 import sys
-import secrets
 from requests.auth import HTTPBasicAuth
-
+wufoo_key = "G0QW-6YXP-H7LX-TN6S"
 
 def get_wufoo_data() -> dict:
     url = "https://comp490project.wufoo.com/api/v3/forms/2023-ultimate-frisbee-tournament/entries/json"
     response = requests.get(url, auth=HTTPBasicAuth(wufoo_key, "pass"))
-    if response.status_code != 200:  # if we don't get an ok response we have trouble
+    if response.status_code != 200:  # if we don't get an ok response we have trouble# import secrets
         print(
             f"Failed to get data, response code:{response.status_code} and error message: {response.reason} "
         )
